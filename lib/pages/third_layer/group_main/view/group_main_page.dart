27285b58438ae2d2d54b46701977b5f81ac1ba72
GemperC <tom.cohen99@gmail.com>
2022-09-14 01:44:25 +0300

    add group key button and copy function to clipboard

--- lib/pages/third_layer/group_main/view/group_main_page.dart
+++ lib/pages/third_layer/group_main/view/group_main_page.dart
@@ -3,10 +3,7 @@ import 'package:dronebag/app.dart';
 import 'package:dronebag/config/font_size.dart';
 import 'package:dronebag/config/theme_colors.dart';
 import 'package:dronebag/domain/group_repository/group_repository.dart';
-import 'package:dronebag/pages/third_layer/group_main/widgets/getGroupKey.dart';
-import 'package:dronebag/pages/third_layer/group_main/widgets/getGroupName.dart';
 import 'package:dronebag/widgets/main_button_2.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -26,90 +23,114 @@ class _MyGroupPageState extends State<MyGroupPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ThemeColors.scaffoldBgColor,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GetGroupInfo(groupID: widget.groupID),
-              SizedBox(height: 80),
-              Center(
-                child: Text(
-                  "Actions",
-                  style: GoogleFonts.poppins(
-                    color: ThemeColors.greyTextColor,
-                    fontSize: FontSize.large,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              MainButton2(
-                  text: 'Group Key',
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => Container(
-                        height: 20,
-                        width: 20,
-                        child: AlertDialog(
-                          title: Center(child: Text('Group Key')),
-                          content: Container(
-                            height: 40,
-                            child: Center(
-                              child: GetGroupKey(
-                                groupID: widget.groupID,
+    return FutureBuilder<Group?>(
+        future: fetchGroup(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final group = snapshot.data;
+
+            return group == null
+                ? CircularProgressIndicator()
+                : Scaffold(
+                    appBar: AppBar(
+                      backgroundColor: ThemeColors.scaffoldBgColor,
+                    ),
+                    body: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              group.name,
+                              style: GoogleFonts.poppins(
+                                color: ThemeColors.whiteTextColor,
+                                fontSize: FontSize.xxLarge,
+                                fontWeight: FontWeight.w600,
                               ),
                             ),
-                          ),
-                          actions: [
-                            TextButton(
-                                onPressed: () => {
-                                      Clipboard.setData(ClipboardData(
-                                              text: getGroupKey(readGroup())))
-                                          .then((value) {
-                                        Utils.showSnackBarWithColor(
-                                            'Key has been copied', Colors.blue);
-                                      })
-                                    },
-                                child: Text('Copy')),
-                            TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text('Back')),
+                            SizedBox(height: 80),
+                            Center(
+                              child: Text(
+                                "Actions",
+                                style: GoogleFonts.poppins(
+                                  color: ThemeColors.greyTextColor,
+                                  fontSize: FontSize.large,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            MainButton2(
+                                text: 'Group Key',
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => Container(
+                                      height: 20,
+                                      width: 20,
+                                      child: AlertDialog(
+                                        title: Center(child: Text('Group Key')),
+                                        content: Container(
+                                          height: 40,
+                                          child: Center(
+                                            child: Text(
+                                              group.key,
+                                              style: GoogleFonts.poppins(
+                                                color: Colors.black,
+                                                fontSize: FontSize.large,
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () => {
+                                                    Clipboard.setData(
+                                                            ClipboardData(
+                                                                text: group.key
+                                                                    .toString()))
+                                                        .then((value) {
+                                                      Utils.showSnackBarWithColor(
+                                                          'Key has been copied',
+                                                          Colors.blue);
+                                                    })
+                                                  },
+                                              child: Text('Copy')),
+                                          TextButton(
+                                              onPressed: () =>
+                                                  Navigator.pop(context),
+                                              child: Text('Back')),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }),
+                            SizedBox(height: 20),
+                            MainButton2(text: 'Drones', onPressed: () {}),
+                            SizedBox(height: 20),
+                            MainButton2(text: 'Fly A Drone', onPressed: () {}),
+                            SizedBox(height: 20),
+                            MainButton2(text: 'Members', onPressed: () {}),
                           ],
                         ),
                       ),
-                    );
-                  }),
-              SizedBox(height: 20),
-              MainButton2(text: 'Drones', onPressed: () {}),
-              SizedBox(height: 20),
-              MainButton2(text: 'Fly A Drone', onPressed: () {}),
-              SizedBox(height: 20),
-              MainButton2(text: 'Members', onPressed: () {}),
-            ],
-          ),
-        ),
-      ),
-    );
+                    ),
+                  );
+          } else {
+            return Scaffold();
+          }
+        });
   }
 
-    readGroup() async {
-    final groupDoc = FirebaseFirestore.instance
-        .collection('groups').doc(widget.groupID);
+  Future<Group?> fetchGroup() async {
+    final groupDoc =
+        FirebaseFirestore.instance.collection('groups').doc(widget.groupID);
     final snapshot = await groupDoc.get();
 
     if (snapshot.exists) {
       return Group.fromJson(snapshot.data()!);
     }
   }
-
-  String getGroupKey(Group group) => group.key;
-
 }
-
