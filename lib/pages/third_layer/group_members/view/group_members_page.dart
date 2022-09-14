8d67f347ae0a25841994de5452e78fc57b797d29
GemperC <tom.cohen99@gmail.com>
2022-09-14 19:13:57 +0300

    add assgin admin

--- lib/pages/third_layer/group_members/view/group_members_page.dart
+++ lib/pages/third_layer/group_members/view/group_members_page.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dronebag/config/font_size.dart';
 import 'package:dronebag/config/theme_colors.dart';
 import 'package:dronebag/domain/group_repository/group_repository.dart';
+import 'package:dronebag/domain/user_repository/src/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -44,8 +45,7 @@ class _GroupMembersState extends State<GroupMembers> {
                     itemCount: group.users.length,
                     itemBuilder: (context, index) {
                       final member = group.users[index];
-                      return buildMember(member);
-                      
+                      return groupMember(member, group);
                     },
                   );
                 } else if (snapshot.hasError) {
@@ -60,29 +60,21 @@ class _GroupMembersState extends State<GroupMembers> {
     );
   }
 
-  Widget buildMember(String member) => ListTile(
-          title: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 80,
-            width: 450,
-            decoration: BoxDecoration(
-                color: ThemeColors.primaryColor,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Center(
-              child: Text(
-                member,
-                style: GoogleFonts.poppins(
-                  color: ThemeColors.whiteTextColor,
-                  fontSize: FontSize.xxLarge,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ));
+  Widget groupMember(String member, Group group) {
+    return FutureBuilder<UserData?>(
+      future: fetchUser(member),
+      builder: ((context, snapshot) {
+        if (snapshot.hasData) {
+          final user = snapshot.data;
+
+          return user == null
+              ? CircularProgressIndicator()
+              : buildMemberTile(user, group);
+        }
+        return CircularProgressIndicator();
+      }),
+    );
+  }
 
   Future<Group?> fetchGroup() async {
     final groupDoc =
@@ -92,5 +84,50 @@ class _GroupMembersState extends State<GroupMembers> {
     if (snapshot.exists) {
       return Group.fromJson(snapshot.data()!);
     }
+  }
+
+  Future<UserData?> fetchUser(String userEmail) async {
+    final userDoc =
+        FirebaseFirestore.instance.collection('users').doc(userEmail);
+    final snapshot = await userDoc.get();
+
+    if (snapshot.exists) {
+      return UserData.fromJson(snapshot.data()!);
+    }
+  }
+
+  Widget buildMemberTile(UserData user, Group group) {
+    return ListTile(
+        onTap: (() {
+          
+          final groupDoc = FirebaseFirestore.instance
+              .collection('groups')
+              .doc(widget.groupID);
+          groupDoc.update({
+            'Group_Admins': FieldValue.arrayUnion([user.email])
+          });
+        }),
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 80,
+              width: 450,
+              decoration: BoxDecoration(
+                  color: ThemeColors.primaryColor,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Center(
+                child: Text(
+                  "${user.fullName} \n${user.email}",
+                  style: GoogleFonts.poppins(
+                    color: ThemeColors.whiteTextColor,
+                    fontSize: FontSize.xxLarge,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
