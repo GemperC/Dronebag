5a31e45d52056102bae4bc07a28df7f871f12f01
GemperC <tom.cohen99@gmail.com>
2022-09-19 00:35:48 +0300

    add dialog for adding new drones

--- lib/pages/third_layer/group__drones/view/group_drones_page.dart
+++ lib/pages/third_layer/group__drones/view/group_drones_page.dart
@@ -9,6 +9,8 @@ import 'package:dronebag/pages/third_layer/group_members/view/view.dart';
 import 'package:dronebag/widgets/main_button_2.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:intl/intl.dart';
 
 class GroupDrones extends StatefulWidget {
   final String groupID;
@@ -22,19 +24,46 @@ class GroupDrones extends StatefulWidget {
 }
 
 class _GroupDronesState extends State<GroupDrones> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController serial_numberController = TextEditingController();
+  final TextEditingController flight_timeController = TextEditingController();
+  final TextEditingController hours_till_maintenaceController =
+      TextEditingController();
+  final TextEditingController maintenanceController = TextEditingController();
+  final TextEditingController date_boughtController = TextEditingController();
+  final double sizedBoxHight = 16;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-            "Group Drones List",
-            style: GoogleFonts.poppins(
-              color: ThemeColors.whiteTextColor,
-              fontSize: FontSize.xxLarge,
-              fontWeight: FontWeight.w600,
-            ),
+        backgroundColor: ThemeColors.scaffoldBgColor,
+        title: Text(
+          "Group Drones List",
+          style: GoogleFonts.poppins(
+            color: ThemeColors.whiteTextColor,
+            fontSize: FontSize.xxLarge,
+            fontWeight: FontWeight.w600,
           ),
-          backgroundColor: ThemeColors.scaffoldBgColor),
+        ),
+        actions: [
+          TextButton(
+            child: Text(
+              'Add New\nDrone',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                color: Colors.blue,
+                fontSize: FontSize.medium,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            onPressed: () {
+              addDroneDialog();
+            },
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
             padding: const EdgeInsets.all(30),
@@ -74,8 +103,7 @@ class _GroupDronesState extends State<GroupDrones> {
 //build the tile of the drone
   Widget buildDroneTile(Drone drone) {
     return ListTile(
-        title: Center(
-      child: Padding(
+      title: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
           height: 80,
@@ -99,8 +127,232 @@ flight time: ${drone.flight_time}
           ),
         ),
       ),
-    ));
+    );
   }
+
+  Future addDroneDialog() {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: ThemeColors.scaffoldBgColor,
+        scrollable: true,
+        title: Text(
+          "Add New Drone",
+          style: GoogleFonts.poppins(
+            color: ThemeColors.whiteTextColor,
+            fontSize: FontSize.large,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        content: Container(
+          width: 300,
+          child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: nameController,
+                    validator: (value) {
+                      if (nameController.text.isEmpty)
+                        return "This field can't be empty";
+                    },
+                    style: GoogleFonts.poppins(
+                      color: ThemeColors.whiteTextColor,
+                    ),
+                    keyboardType: TextInputType.name,
+                    cursorColor: ThemeColors.primaryColor,
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.textFieldBgColor,
+                      filled: true,
+                      hintText: "Drone Name",
+                      hintStyle: GoogleFonts.poppins(
+                        color: ThemeColors.textFieldHintColor,
+                        fontSize: FontSize.small,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(18)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizedBoxHight),
+                  TextFormField(
+                    controller: serial_numberController,
+                    validator: (value) {
+                      if (nameController.text.isEmpty)
+                        return "This field can't be empty";
+                    },
+                    style: GoogleFonts.poppins(
+                      color: ThemeColors.whiteTextColor,
+                    ),
+                    keyboardType: TextInputType.name,
+                    cursorColor: ThemeColors.primaryColor,
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.textFieldBgColor,
+                      filled: true,
+                      hintText: "Serial number",
+                      hintStyle: GoogleFonts.poppins(
+                        color: ThemeColors.textFieldHintColor,
+                        fontSize: FontSize.small,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(18)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizedBoxHight),
+                  TextFormField(
+                    controller: flight_timeController,
+                    validator: (value) {
+                      if (nameController.text.isEmpty)
+                        return "This field can't be empty";
+                    },
+                    style: GoogleFonts.poppins(
+                      color: ThemeColors.whiteTextColor,
+                    ),
+                    keyboardType: TextInputType.number,
+                    cursorColor: ThemeColors.primaryColor,
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.textFieldBgColor,
+                      filled: true,
+                      hintText: "Flight Time",
+                      hintStyle: GoogleFonts.poppins(
+                        color: ThemeColors.textFieldHintColor,
+                        fontSize: FontSize.small,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(18)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizedBoxHight),
+                  TextFormField(
+                    controller: maintenanceController,
+                    validator: (value) {
+                      if (nameController.text.isEmpty)
+                        return "This field can't be empty";
+                    },
+                    style: GoogleFonts.poppins(
+                      color: ThemeColors.whiteTextColor,
+                    ),
+                    keyboardType: TextInputType.number,
+                    cursorColor: ThemeColors.primaryColor,
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.textFieldBgColor,
+                      filled: true,
+                      hintText: "Maintnenace cycle in hours",
+                      hintStyle: GoogleFonts.poppins(
+                        color: ThemeColors.textFieldHintColor,
+                        fontSize: FontSize.small,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(18)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizedBoxHight),
+                  DateTimeField(
+                    format: DateFormat('yyyy-MM-dd'),
+                    controller: date_boughtController,
+                    onShowPicker: ((context, currentValue) {
+                      return showDatePicker(
+                          context: context,
+                          firstDate: DateTime(1900),
+                          initialDate: currentValue ?? DateTime.now(),
+                          lastDate: DateTime(2100));
+                    }),
+                    validator: (value) {
+                      if (nameController.text.isEmpty)
+                        return "This field can't be empty";
+                    },
+                    style: GoogleFonts.poppins(
+                      color: ThemeColors.whiteTextColor,
+                    ),
+                    keyboardType: TextInputType.datetime,
+                    cursorColor: ThemeColors.primaryColor,
+                    decoration: InputDecoration(
+                      fillColor: ThemeColors.textFieldBgColor,
+                      filled: true,
+                      hintText: "Date bought",
+                      hintStyle: GoogleFonts.poppins(
+                        color: ThemeColors.textFieldHintColor,
+                        fontSize: FontSize.small,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(18)),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel')),
+          TextButton(
+              onPressed: () {
+                createDrone();
+              },
+              child: Text('Add Drone')),
+        ],
+      ),
+    );
+  }
+
+//create a new drone and add it to the group
+  Future createDrone() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    } else {
+      final docDrone = FirebaseFirestore.instance
+          .collection('groups')
+          .doc(widget.groupID)
+          .collection('drones')
+          .doc();
+      final drone = Drone(
+        name: nameController.text.trim(),
+        serial_number: serial_numberController.text,
+        flight_time: int.parse(flight_timeController.text),
+        id: docDrone.id,
+        hours_till_maintenace: hoursUntillMaintnance(),
+        maintenance: int.parse(maintenanceController.text),
+        date_added: DateTime.now(),
+        date_bought: DateTime.parse(date_boughtController.text),
+      );
+
+      final json = drone.toJson();
+      await docDrone.set(json);
+      Utils.showSnackBarWithColor(
+          'Drone has been added to the group', Colors.blue);
+      Navigator.pop(context);
+    }
+  }
+
+//find how many hours left till drone maintnance
+int hoursUntillMaintnance(){
+  int flightTime = int.parse(flight_timeController.text);
+  int maintnanceCycle = int.parse(maintenanceController.text);
+  int hoursLeftToMaintnance;
+
+ if (flightTime == 0) {return maintnanceCycle;}
+ hoursLeftToMaintnance = (maintnanceCycle - flightTime%maintnanceCycle)%200;
+  return hoursLeftToMaintnance; 
+}
+
+
 
   Widget fetchgroupDrones(
       String member, Group group, bool isAdmin, String loggedUserEmail) {
